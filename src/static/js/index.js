a5ca91098f82f396c2d9cb5e8d6cf1fc38f1f032
chan056 <374029208@qq.com>
2017-10-18 21:14:36 +0800

    t

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -1,4 +1,4 @@
-Vue.component('todo-item', {
+/* Vue.component('todo-item', {
     props: ['todo'],
     template: '<li><a :href="todo.link">{{ todo.text }}</a></li>'
 })
@@ -12,4 +12,16 @@ var app = new Vue({
             { id: 2, text: '随便其他什么人吃的东西' }
         ]
     }
-})
+}) */
+
+var data = {a: 1};
+var vm = new Vue({
+    el: '#example',
+    data: data
+});
+
+console.log(vm.$data === data, vm.$el);
+
+vm.$watch('a', function(){
+    console.log('a change from '+arguments[1]+' to ' + arguments[0]);
+});
