9ea82b60cff439388a035dd9f48abbbfa0330947
chenyi <chenyi@zjcap.cn>
2018-06-01 17:02:29 +0800

    180601

--- src/static/js/source/g-component.js
+++ src/static/js/source/g-component.js
@@ -1,24 +1,24 @@
 module.exports = function(){
-    let tagCreatorConstructor = Vue.extend({
-        template: `
-            <el-dialog v-bind:title="config.title" :visible.sync="config.visibility">
-                <el-form>
+    // let tagCreatorConstructor = Vue.extend({
+    //     template: `
+    //         <el-dialog v-bind:title="config.title" :visible.sync="config.visibility">
+    //             <el-form>
                     
-                </el-form>
-                <div slot="footer" class="dialog-footer">
-                    <el-button @click="config.visibility = false">取 消</el-button>
-                    <el-button type="primary" @click="config.visibility = false">确 定</el-button>
-                </div>
-            </el-dialog>`,
-        data: function () {
-            return {
+    //             </el-form>
+    //             <div slot="footer" class="dialog-footer">
+    //                 <el-button @click="config.visibility = false">取 消</el-button>
+    //                 <el-button type="primary" @click="config.visibility = false">确 定</el-button>
+    //             </div>
+    //         </el-dialog>`,
+    //     data: function () {
+    //         return {
                 
-            }
-        },
-        props:['config'],
-    });
+    //         }
+    //     },
+    //     props:['config'],
+    // });
     
-    Vue.component('tagCreator', tagCreatorConstructor);
+    // Vue.component('tagCreator', tagCreatorConstructor);
     
     // 时间的转换 如 2周之前 1天之前
     Vue.component('UpdateTime', {
