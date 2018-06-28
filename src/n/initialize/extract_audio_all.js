926c60cc5ad300fb0a588277f58ab2cf59d8174a
chan056 <chenyi056@163.com>
2018-06-28 23:54:44 +0800

     提取所有视频的音频

--- src/n/initialize/extract_audio_all.js
+++ src/n/initialize/extract_audio_all.js
@@ -1,31 +1,52 @@
 
 var fs = require('fs');
 var path = require('path');
+const exec = require('child_process').exec;
 
-var root = path.resolve(__dirname, '../../static/multimedia/ts');
+var tsRoot = path.resolve(__dirname, '../../static/multimedia/ts');
 
-fs.readdir(root, function(err, files){
+fs.readdir(tsRoot, function(err, files){
+    let vIds = [];
     files.forEach(function(file){
-        var videoRoot = path.resolve(root, file)
-        fs.stat(videoRoot, function(err, stats){
-            if(stats.isDirectory()){
-                let vId = path.basename(videoRoot);
-                let videoStorePath = path.resolve(__dirname, '../../static/multimedia/pristine_v/'+vId+'.mp4');
-
-                extractAudio(videoStorePath, root + '/' + vId);
-            }
-        })
+        var videoRoot = path.resolve(tsRoot, file)
+        let stat = fs.statSync(videoRoot);
+        if(stat.isDirectory()){
+            vIds.push(path.basename(videoRoot));
+        }    
     })
+
+    // console.log(vIds);
+    extract(0);
+
+    function extract(i){
+        let vId = vIds[i];
+        if(!vId)
+            return;
+        console.log(i, vId)
+        let videoStorePath = path.resolve(__dirname, '../../static/multimedia/pristine_v/'+vId+'.mp4');
+
+        if(fs.existsSync(videoStorePath)){
+            extractAudio(videoStorePath, tsRoot + '/' + vId, function(){
+                extract(++i);
+            });
+        }
+    }
 })
 
-function extractAudio(videoStorePath, tsDir){
-    let cmd = `ffmpeg -i ${videoStorePath} -q:a 0 -map a ${tsDir}/audio.mp3`;
+function extractAudio(videoStorePath, tsDir, fn){
+    let target = `${tsDir}/audio.mp3`;
+    if(fs.existsSync(target)){
+        fn && fn();
+        return;
+    }
 
-    require('child_process').exec(cmd, function(error){
+    let cmd = `ffmpeg -i ${videoStorePath} -q:a 0 -map a ${target}`;
+
+    exec(cmd, function(error){
         if(error){
             console.log(error);
         }
 
-        console.log(videoStorePath)
+        fn && fn();
     })
 }
