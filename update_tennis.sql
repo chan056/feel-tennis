d5e4007385705bbc8223f1cba39aac5650725894
chan056 <chenyi056@163.com>
2019-01-31 15:28:23 +0800

    更新表结构

--- update_tennis.sql
+++ update_tennis.sql
@@ -1,3 +1,17 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : yitube
+Source Server Version : 50717
+Source Host           : localhost:3306
+Source Database       : tennis
+
+Target Server Type    : MYSQL
+Target Server Version : 50717
+File Encoding         : 65001
+
+Date: 2019-01-31 15:27:47
+*/
 
 SET FOREIGN_KEY_CHECKS=0;
 
@@ -7,10 +21,13 @@ SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS `athlete`;
 CREATE TABLE `athlete` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `sport_id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
-  `en_name` varchar(255) DEFAULT NULL,
+  `name_en` varchar(255) DEFAULT NULL,
   `gender` tinyint(1) NOT NULL DEFAULT '1' COMMENT '1男 2女 3其他',
   `ranking` mediumint(9) DEFAULT NULL,
+  `prev_ranking` mediumint(9) DEFAULT NULL,
+  `point` mediumint(9) DEFAULT NULL,
+  `country` varchar(255) DEFAULT NULL,
+  `state_abbreviation` varchar(10) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=201 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=371 DEFAULT CHARSET=utf8;
