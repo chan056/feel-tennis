b39401faae5fe8a93c29bd89a236c51165aee32c
chan056 <chenyi056@163.com>
2019-02-18 17:45:00 +0800

    190218

--- update_tennis.sql
+++ update_tennis.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50717
 File Encoding         : 65001
 
-Date: 2019-02-15 17:51:42
+Date: 2019-02-18 17:44:23
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -20,12 +20,11 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 DROP TABLE IF EXISTS `athlete`;
 CREATE TABLE `athlete` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_tennis_com` int(11) DEFAULT NULL COMMENT 'id in tennis.com',
-  `name` varchar(255) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
   `name_en` varchar(255) DEFAULT NULL,
   `nickname` varchar(50) DEFAULT NULL,
-  `gender` tinyint(1) NOT NULL DEFAULT '1' COMMENT '1男 2女 3其他',
+  `gender` tinyint(1) DEFAULT NULL COMMENT '1男 2女 3其他',
   `ranking` mediumint(9) DEFAULT NULL,
   `prev_ranking` mediumint(9) DEFAULT NULL,
   `point` mediumint(9) DEFAULT NULL,
@@ -38,9 +37,11 @@ CREATE TABLE `athlete` (
   `height` double DEFAULT NULL COMMENT 'cm',
   `weight` double DEFAULT NULL COMMENT 'kg',
   `plays` tinyint(1) DEFAULT NULL COMMENT '右手0(默认） 左手1 ',
-  `experience` tinyint(4) DEFAULT '0' COMMENT '转职业选手年数',
+  `experience` tinyint(4) DEFAULT NULL COMMENT '转职业选手年数',
   `ytd_win_single` tinyint(4) DEFAULT NULL,
   `ytd_win_double` tinyint(4) DEFAULT NULL COMMENT '年初至今赢的次数',
   `website` varchar(255) DEFAULT NULL COMMENT '个人站点',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=368 DEFAULT CHARSET=utf8;
+  `ranking_expire` datetime DEFAULT NULL COMMENT 'ranking date expire time',
+  `stat_expire` datetime DEFAULT NULL COMMENT 'stat data expire time',
+  UNIQUE KEY `idd` (`id_tennis_com`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
