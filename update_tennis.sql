bb7169c8ec10dd53f1daf0956762d05637d95e11
chan056 <chenyi056@163.com>
2019-04-02 17:51:13 +0800

    190402

--- update_tennis.sql
+++ update_tennis.sql
@@ -1,13 +1,12 @@
-DROP TABLE IF EXISTS `translation`;
-CREATE TABLE `translation` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `en` text,
-  `zh` text,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-
-
-athlete
-  is_top
-  player_image_small
-  top_expire
+DROP TABLE IF EXISTS `tournament`;
+CREATE TABLE `tournament` (
+  `sid` int(11) NOT NULL,
+  `status` tinyint(1) DEFAULT NULL COMMENT '-1 recent, 0 current, 1 upcoming',
+  `name` varchar(255) DEFAULT NULL,
+  `link` varchar(255) DEFAULT NULL,
+  `year` smallint(6) DEFAULT NULL,
+  `start_time` bigint(20) DEFAULT NULL,
+  `end_time` bigint(20) DEFAULT NULL,
+  `expire` datetime DEFAULT NULL,
+  PRIMARY KEY (`sid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
